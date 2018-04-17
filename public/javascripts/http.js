42e1f8bf08b97a530bb0f39bfd8b002f96c8929b
Allison12345 <2562392373@qq.com>
2018-04-17 15:00:37 +0800

    http.js

--- public/javascripts/http.js
+++ public/javascripts/http.js
@@ -2,13 +2,17 @@ document.body.onload = function() {
   console.log('page down')
 }
 document.getElementById('button').onclick = function() {
+  var username = document.getElementById('username').value
+  var password = document.getElementById('password').value
+  console.log(username)
+  console.log(password)
   const xhr = new XMLHttpRequest()
-  xhr.open('GET', '/data.json', true)
+  xhr.open('POST', '/login', true)
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      const data =  JSON.parse(xhr.response)
-      console.log(data)
+      const data = JSON.parse(xhr.response)
+      // console.log(data)
     }
   }
-  xhr.send()
+  xhr.send({username,password})
 }
